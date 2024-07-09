82f33485e275b3732ff24e2a0114ed54de966142
Mrazakos <mrazakos05@gmail.com>
2024-07-09 14:46:51 +0200

    added scroll animation to the list of tracks

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let ol = document.getElementsByClassName("category");
 const queryString = window.location.search;
 const genre = new URLSearchParams(queryString).get("genre");
 for(const li of ol){
-    console.log(li);
+
     if(li.id == genre){
         if(!li.classList.contains("selected")){
             li.classList.replace("non-selected", "selected");
@@ -15,30 +15,13 @@ for(const li of ol){
         }
     }
 }
-console.log(genre);
+
 
 //adding and deleting categories
 
 function showcaseCategory(category){
     window.location = "?genre=" + category;
 }
-
-function hideCategory(category){
-    let hideCategory = document.getElementById(category);
-    hideCategory.classList.replace("selected", "non-selected");
-    hideCategory.setAttribute( "onClick", "javascript: showcaseCategory('" + category + "');" );
-    console.log(showcaseCategory);
-    const modifiedCategories = [];
-    let index = 0;
-    for(i = 0; i < categories.length; i++){
-        if(categories[i] != category){
-            modifiedCategories[index++] = categories[i];
-        }
-    }
-    categories = modifiedCategories;
-
-}
-
 
 
 
--- script.js
+++ script.js
@@ -1,37 +1,10 @@
 //displaying desired categories
 
 const clientId = "96b556fab4bc4ea6a3a37a8dbdb0b66b"; // Replace with your client ID
-const params = new URLSearchParams(window.location.search);
+
 const clientSecret = "c55e2db3963147e8aa1ed3a014c08d1e";
-const code = params.get("code");
 
-
-    
-const accessToken = await getAccessToken(clientId, clientSecret);
-const queryString = window.location.search;
-const genre = new URLSearchParams(queryString).get("genre");
-let top50 = undefined;
-switch(genre){
-    case "all": top50 = await getMostPopular(accessToken, "37i9dQZEVXbMDoHDwVN2tF");
-    break;
-    case "pop": top50 = await getMostPopular(accessToken, "37i9dQZF1DX2vTOtsQ5Isl");
-    break;
-    case "hiphop": top50 = await getMostPopular(accessToken, "5TZkls9cEOzWDR6qCxwDot");
-    break;
-    case "rnb": top50 = await getMostPopular(accessToken, "3SsPWyQnAh1ccWEXZPYaCY");
-    break;
-    case "rock": top50 = await getMostPopular(accessToken, "1k59k1PJIk5ZJ6ppbFuzgS");
-    break;
-    default: displayNothing(); 
-}
-
-top50 = top50["items"];
-console.log(top50);
-displaySongs(top50);
-
-    
-
-
+displaySongs(50);
 
 export async function getAccessToken(clientId, clientSecret) {
     const verifier = localStorage.getItem("verifier");
@@ -71,11 +44,36 @@ function displayNothing(){
     songs.innerHTML += html;
 }
 
-function displaySongs(top50){
+export async function displaySongs(numberOfSongs){
+    const accessToken = await getAccessToken(clientId, clientSecret);
+    const queryString = window.location.search;
+    const genre = new URLSearchParams(queryString).get("genre");
+    let top50 = undefined;
+    switch(genre){
+        case "all": top50 = await getMostPopular(accessToken, "37i9dQZEVXbMDoHDwVN2tF");
+        break;
+        case "pop": top50 = await getMostPopular(accessToken, "37i9dQZF1DX2vTOtsQ5Isl");
+        break;
+        case "hiphop": top50 = await getMostPopular(accessToken, "5TZkls9cEOzWDR6qCxwDot");
+        break;
+        case "rnb": top50 = await getMostPopular(accessToken, "3SsPWyQnAh1ccWEXZPYaCY");
+        break;
+        case "rock": top50 = await getMostPopular(accessToken, "1k59k1PJIk5ZJ6ppbFuzgS");
+        break;
+        default: displayNothing(); 
+    }
+    top50 = top50["items"];
+    const ol = document.querySelector("ol");
+    ol.innerHTML = "";
+    let num = 0
     for(const song of top50){
+        if(num == numberOfSongs){
+            break;
+        }
         const track = song.track;
+        
         let html = `
-        <li>
+        <li class="hidden">
             <div class="song">
                 <div class="songData">
                     <h3 class="songName">${track.name}</h3>
@@ -89,15 +87,33 @@ function displaySongs(top50){
                 html += artist.name;
             }
             
-        }
-
-                    
+        }     
         html += `   </h4>
                     <p class="album">${track.album.name}</p>
                 </div>
             </div>
         </li>`;
-        const ol = document.querySelector("ol");
         ol.innerHTML += html;
+        num++;
     }
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry.target);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else{
+                entry.target.classList.remove('show');
+            }
+        });
+    }, {
+        // Optional: Customize the root margin (adjust as needed)
+        rootMargin: '10px',
+        // Optional: Set a threshold for intersection (0.5 means 50% visibility)
+        threshold: 0.2,
+    });
+    
+    const hiddenElements = document.querySelectorAll('li.hidden');
+    console.log(hiddenElements);
+    hiddenElements.forEach((el) => observer.observe(el));
+    
 }
